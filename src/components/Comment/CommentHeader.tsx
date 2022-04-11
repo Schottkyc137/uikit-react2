9fe6a68bdff7364571be397a1c5b36917cb5af92
Schottkyc137 <lukas2121@icloud.com>
2022-04-11 21:12:22 +0200

    Add Forward Ref

--- src/components/Comment/CommentHeader.tsx
+++ src/components/Comment/CommentHeader.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { extendClasses } from '../util/class-names';
 import { CommentHeaderProps } from './CommentProps';
 
-export function CommentHeader({ className, ...rest }: CommentHeaderProps) {
-  return (
-    <header
-      className={extendClasses(className, 'uk-comment-header')}
-      {...rest}
-    />
-  );
-}
+export const CommentHeader = React.forwardRef(
+  (props: CommentHeaderProps, ref: React.ForwardedRef<HTMLElement>) => {
+    const { className, ...rest } = props;
+    return (
+      <header
+        ref={ref}
+        className={extendClasses(className, 'uk-comment-header')}
+        {...rest}
+      />
+    );
+  }
+);
--- src/components/Comment/CommentTitle.tsx
+++ src/components/Comment/CommentTitle.tsx
@@ -2,8 +2,15 @@ import { CommentTitleProps } from './CommentProps';
 import { extendClasses } from '../util/class-names';
 import React from 'react';
 
-export function CommentTitle({ className, ...rest }: CommentTitleProps) {
-  return (
-    <h4 className={extendClasses(className, 'uk-comment-title')} {...rest} />
-  );
-}
+export const CommentTitle = React.forwardRef(
+  (props: CommentTitleProps, ref: React.ForwardedRef<HTMLHeadingElement>) => {
+    const { className, ...rest } = props;
+    return (
+      <h4
+        ref={ref}
+        className={extendClasses(className, 'uk-comment-title')}
+        {...rest}
+      />
+    );
+  }
+);
