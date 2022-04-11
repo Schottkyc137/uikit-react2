009ffd133eee4ad7bfa40876f331db8a41d9143c
Schottkyc137 <lukas2121@icloud.com>
2022-04-11 20:38:27 +0200

    Remove duplicate export

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -2,7 +2,6 @@ export * from './Card';
 export * from './CardBadge';
 export * from './CardBody';
 export * from './CardFooter';
-export * from './CardFooter';
 export * from './CardHeader';
 export * from './CardMedia';
 export * from './CardTitle';
